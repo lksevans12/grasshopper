65dc4a856e15bf733f8e4ee9d19f0e3aa153aa90
prithajnath <prithajnath@gmail.com>
2016-06-19 07:20:13 -0400

    Added basic dashboard

--- mvp/config/routes.rb
+++ mvp/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'tutor/show'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
