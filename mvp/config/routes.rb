76d632859412b6b2a3a6b3436fff67dd112671ef
lksevans12 <lksevans12@gmail.com>
2016-06-19 12:31:49 -0400

    add logo and reformat nav bar to fit logo

--- mvp/config/routes.rb
+++ mvp/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'tutor/show' => 'tutor#show'
+  get 'user/:id' => 'user#show',:as => :user_show, :via => :get
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
   root "welcome#index"
 
-  get 'user/:id' => 'user#show'
+  
   
 end
