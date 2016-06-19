6846d33f4730f684c900ee94dc38dd562c44db26
lksevans12 <lksevans12@gmail.com>
2016-06-19 14:09:43 -0400

    update profile page

--- mvp/config/routes.rb
+++ mvp/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'tutor/show' => 'tutor#show'
   get 'user/:id' => 'user#show',:as => :user_show, :via => :get
 
+
+
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
