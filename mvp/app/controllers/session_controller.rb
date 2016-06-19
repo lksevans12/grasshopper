1c1b8378c697ecf6cfff516c9c994359e13b6717
lksevans12 <lksevans12@gmail.com>
2016-06-19 15:40:33 -0400

    basic scheduling mockup

---
+++ mvp/app/controllers/session_controller.rb
@@ -0,0 +1,6 @@
+class SessionController < ApplicationController
+
+	def schedule
+	end
+
+end
--- mvp/config/routes.rb
+++ mvp/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'tutor/show' => 'tutor#show'
   get 'user/:id' => 'user#show',:as => :user_show, :via => :get
 
+  get 'schedule-session' => 'session#schedule',:as => :session_schedule, :via => :get
+
 
 
 
